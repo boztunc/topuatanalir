797f5bbd38fa4aa0700ba90a5eb0946a8032e289
boztunc <oztunc.beytullah@gmail.com>
2019-08-29 17:31:37 +0300

    update

--- routes/api.js
+++ routes/api.js
@@ -5,23 +5,39 @@ const rp = require('request-promise').defaults({ encoding: 'latin1' });
 const cheerio = require('cheerio');
 const axios = require('axios');
 
-
-const iddaalobisi = "http://iddaalobisi.com/avrupada-en-cok-oynanan-maclar.html";
+const IL_avr_maclar = "http://iddaalobisi.com/avrupada-en-cok-oynanan-maclar.html";
+const IL_oraniDusen = "http://iddaalobisi.com/orani-dusen-maclar.html";
 const betistutaFutbol = "http://www.betistuta.de/Futbol.aspx";
 const orandelliBulten = "http://www.betistuta.de/Iddaa.aspx";
 
-router.get('/iddaalobisi', function (req, res) {
-
-    rp(iddaalobisi).then((html) => {
-        var table = cheerio('.sOdList', html).html();
-        res.status(200).send(table);
+router.get('/iddaalobisi-avr-maclar', function (req, res) {
+    iddaalobisi(IL_avr_maclar).then((result) => {
+        if (result.error) {
+            console.log("Avrupada Oynan Maçlar Çekilemedi...");
+            console.log(result);
+            res.status(400);
+        }
+        else {
+            res.status(200).send(result);
+            console.log(result)
+        }
         res.end();
+    });
+});
 
-    }).catch((err) => {
-        res.status(400).send(err);
-        console.log('iddaalobisi hata ----> ' + err);
+router.get('/iddaalobisi-oran-dusen', function (req, res) {
+    iddaalobisi(IL_oraniDusen).then((result) => {
+        if (result.error) {
+            console.log("Oranı Düşen Maçlar Çekilemedi...");
+            console.log(result);
+            res.status(400);
+        }
+        else {
+            res.status(200).send(result);
+            console.log(result)
+        }
         res.end();
-    })
+    });
 });
 
 router.get('/betistuta-futbol', function (req, res) {
@@ -54,4 +70,12 @@ router.get('/orandelli-bulten', function (req, res) {
     })
 });
 
+
+var iddaalobisi = (url) => {
+    return rp(url).then((html) => {
+        return cheerio('.sOdList', html).html();
+    }).catch((err) => {
+        return err;
+    });
+}
 module.exports = router;
